611d93a15aa25781483d06036da9b062d6d49500
Jon Hough <jon.g.hough@gmail.com>
2014-07-29 01:16:05 +0900

    Large scale refactoring and create shell channel

--- AndroidSSH/src/com/jgh/androidssh/sshutils/ShellController.java
+++ AndroidSSH/src/com/jgh/androidssh/sshutils/ShellController.java
@@ -1,10 +1,16 @@
 package com.jgh.androidssh.sshutils;
 
+import android.os.Handler;
+import android.util.Log;
+import android.widget.EditText;
+
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.JSchException;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by jon on 5/17/14.
@@ -12,9 +18,9 @@ import java.io.OutputStream;
 public class ShellController {
 
 
-    private SessionController mSessionController;
-    private InputStream mIS;
-    private OutputStream mOS;
+    private final SessionController mSessionController;
+    private BufferedReader mBufferedReader;
+    private DataOutputStream mDataOutputStream;
 
     public ShellController(SessionController sessionController){
         mSessionController = sessionController;
@@ -22,22 +28,47 @@ public class ShellController {
 
 
 
-    private class ShellRunnable implements Runnable{
+    public void openShell(Handler handler, EditText editText) throws JSchException, IOException {
+        final Handler myHandler = handler;
+        final EditText myEditText = editText;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
 
-        private Channel mChannel;
-        private InputStream mIS;
-        private OutputStream mOS;
-        @Override
-        public void run(){
-            try{
-                mChannel= ShellController.this.mSessionController.getSession().openChannel("shell");
-                mChannel.setInputStream(mIS);
+                try {
+                    Channel channel = mSessionController.getSession().openChannel("shell");
+                    channel.connect();
+
+                    BufferedReader input;
+                    input = new BufferedReader(new InputStreamReader(channel.getInputStream()));
+                    DataOutputStream output = new DataOutputStream(channel.getOutputStream());
+
+
+                    output.writeBytes("ls"+"\r\n");
+                    output.flush();
+
+
+                    String line = input.readLine();
+                    String result = line + "\n";
+
+                    while ((line = input.readLine()) != null) {
+                        result += line + "\n";
+                        final String rez = line;
+
+                        myHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                myEditText.setText(myEditText.getText().toString() + "\n" + rez);
+                            }
+                        });
+                    }
+
+                } catch (Exception e) {
+                    Log.v("EXECPTION", " EX " + e.getMessage()+"."+e.getCause()+","+e.getClass().toString());
+                }
 
             }
-            catch(JSchException je){
-
-            }
-        }
+        }).start();
     }
 
 }
